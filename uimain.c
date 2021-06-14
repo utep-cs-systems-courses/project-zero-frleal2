c6f9f2d9ea8e12a7c1fec0a0528168b7773b3fe7
student <frleal2@miners.utep.edu>
2021-06-14 13:39:23 -0600

    this version draws the arrrow

--- uimain.c
+++ uimain.c
@@ -7,7 +7,7 @@ int main()
 
   while (1) { // Infinite while loop
 
-    fputs("Select which shape you want to print (Triangle = t, Square = s, Chars = c) or 'q' to quit\n> ", stdout);
+    fputs("Select which shape you want to print (Triangle = t, Square = s, Chars = c, Custom Chars = d, Arrow = a) or 'q' to quit\n> ", stdout);
     fflush(stdout);		/* stdout only flushes automatically on \n */
     int c;
     while ((c = getchar()) == '\n'); /* ignore newlines */
@@ -28,6 +28,15 @@ int main()
       puts("You selected chars:");
       for (char c = 'A'; c < 'D'; c++)
 	print_char_11x16(c);
+      break;
+    case 'd':
+      puts("You selected a custom font chars");
+      for(char c = 'A'; c< 'D'; c++)
+	print_char_8x12(c);
+      break;
+    case 'a':
+      puts("You selected arrow"); //given this command the program will print an arrow
+      print_arrow();
       break;
     case 'q':
       puts("Bye!");
